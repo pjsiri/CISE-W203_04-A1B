70fc82880ad5989670b58705242fd5a7f11d8cfa
bsouthg8 <bsouthg8@gmail.com>
2024-10-04 01:53:24 +1300

    small updates to main, and separated workflow files for backend and frontend

--- frontend/src/components/table/SortableTable.tsx
+++ frontend/src/components/table/SortableTable.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 
-interface SortableTableProps {
-    headers: { key: string; label: string }[];
-    data: any[];
+interface SortableTableProps<T> {
+  headers: { key: keyof T; label: string }[];
+  data: T[];
 }
 
-const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => (
-    <table>
-        <thead>
-            <tr>
-                {headers.map((header) => (
-                    <th key={header.key}>{header.label}</th>
-                ))}
-            </tr>
-        </thead>
-        <tbody>
-            {data.map((row, i) => (
-                <tr key={i}>
-                    {headers.map((header) => (
-                        <td key={header.key}>{row[header.key]}</td>
-                    ))}
-                </tr>
-            ))}
-        </tbody>
-    </table>
+const SortableTable = <T,>({ headers, data }: SortableTableProps<T>) => (
+  <table>
+    <thead>
+      <tr>
+        {headers.map((header) => (
+          <th key={header.key as string}>{header.label}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((row, i) => (
+        <tr key={i}>
+          {headers.map((header) => (
+            <td key={header.key as string}>{String(row[header.key])}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
 );
 
 export default SortableTable;
