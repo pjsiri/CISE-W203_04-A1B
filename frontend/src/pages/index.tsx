5f135518faacc228641ae89e9ca86124c886e752
PJ <104670311+pjsiri@users.noreply.github.com>
2024-10-15 20:27:40 +1300

    Improved home page styling and description. Added sign in button to home.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,33 @@
+import styles from '../styles/Home.module.scss';
+import { signIn } from 'next-auth/react'; // Import the sign-in function
+
 export default function Home() {
     return (
-        <div className="container">
-            <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Software Practice Empirical Evidence Database (SPEED)</h1>
+            <a href="https://emoji.gg/emoji/28649-pepecry2">
+                <img
+                    src="https://cdn3.emoji.gg/emojis/28649-pepecry2.png"
+                    width="64px"
+                    height="64px"
+                    alt="PepeCry2"
+                />
+            </a>
+            <p className={styles.description}>
+                Welcome to SPEED, a searchable database of empirical evidence about software engineering practices and claims.
+                Our goal is to help practitioners make informed decisions based on robust data and insights.
+            </p>
+            <div className={styles.ctaContainer}>
+                <h2 className={styles.ctaTitle}>Get Started</h2>
+                <p>
+                    Explore our collection of articles, submit your own research, or engage with our community of software 
+                    engineering professionals.
+                </p>
+                <button className={styles.ctaButton} onClick={() => signIn()}>
+                    Sign In
+                </button>
+                
+            </div>
         </div>
     );
 }
